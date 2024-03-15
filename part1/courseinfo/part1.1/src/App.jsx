78ac886cdde26b262c98795da604efbb81b2a136
Ivan Tovpeko <ivan.tko35@gmail.com>
2024-03-15 07:27:20 +0000

    finished part1.1 exercise - it is rendering correctly

---
+++ part1/courseinfo/part1.1/src/App.jsx
@@ -0,0 +1,37 @@
+const Header = ({course}) => {
+  return <h1>{course}</h1>
+}
+
+const Content = ({ part, exercises }) => {
+  return <p>
+    {part} {exercises}
+    </p>
+}
+
+const Total = ({exer1, exer2, exer3}) => {
+  return <p>
+    {exer1 + exer2 + exer3}
+  </p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+ 
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exer1={exercises1} exer2={exercises2} exer3={exercises3} />
+    </div>
+  )
+}
+
+export default App
